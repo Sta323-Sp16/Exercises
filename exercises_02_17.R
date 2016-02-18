07097d68cd219d73b817b56f19f9e764430c2599
Colin Rundel <rundel@gmail.com>
2016-02-18 00:54:34 -0500

    Get posters and scores

--- exercises_02_17.R
+++ exercises_02_17.R
@@ -12,5 +12,36 @@ df = data.frame(
 
 df$gross = movies %>% html_nodes("span") %>% html_text()
 
+
+
+
 links = movies %>% html_nodes("a") %>% html_attr("href") %>% paste0("http://imdb.com", .)
 
+subpages = lapply(links, read_html)
+
+get_poster = function(page)
+{
+  res = page %>% 
+          html_nodes("#img_primary img") %>% 
+          html_attr("src")
+  if(length(res) == 0)
+  {
+    res = page %>%
+            html_nodes("#title-overview-widget img") %>%
+            .[1] %>%
+            html_attr("src")
+  }
+  
+  return(res)
+}
+
+df$poster_link = sapply(subpages, get_poster)
+
+get_score = function(page)
+{
+  page %>%
+    html_nodes("strong span") %>% 
+    html_text()
+}
+
+df$score = sapply(subpages, get_score)
