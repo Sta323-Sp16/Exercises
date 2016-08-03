1a501e61a21095867a0a198db6f7c548d9d9df73
Colin Rundel <rundel@gmail.com>
2016-08-03 09:16:37 -0500

    Old stuff?

--- exercises_02_29/Fig1/fig.R
+++ exercises_02_29/Fig1/fig.R
@@ -2,7 +2,7 @@ r = rlnorm(1000)
 
 h = hist(r, plot=FALSE, breaks=c(seq(0,max(r)+1, .1)))
 
-pdf("fig.pdf")
+pdf("fig.pdf", width=7)
 plot(h$counts, log="xy", pch=20, col="blue",
      main="Log-normal distribution",
      xlab="Value", ylab="Frequency")
